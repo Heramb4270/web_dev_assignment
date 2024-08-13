7a2fbc00b2d8e97dbe2fc6aa5f45d0fa58c5d17c
Heramb Bhoodhar <heramb.bhoodhar@gmail.com>
2024-08-13 21:56:38 +0530

    WT Assignment

---
+++ TO DO List/todolist/src/WebDev.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function WebDev() {
+    return (
+        <div>
+
+            <h1>Web Development</h1>
+            <p>Web development is the building and maintenance of websites; it’s the work that happens behind the scenes to make a website look great, work fast and perform well with a seamless user experience.</p>
+            <p>Web developers, or ‘devs’, do this by using a variety of coding languages. The languages they use depends on the types of tasks they are preforming and the platforms on which they are working.</p>
+            <p>The most common languages used in web development are HTML, CSS, and JavaScript. These languages are used to build the structure of a website, the style and design of a website, and the interactivity of a website, respectively.</p>
+            <p>Web developers can also use other languages such as Python, Ruby, and PHP to build more complex features on a website. </p>
+            <p>Web development can be broken down into three main specializations: front-end, back-end, and full-stack.</p>
+            <p>Front-end developers are responsible for the parts of a website that users see and interact with. They use HTML, CSS, and JavaScript to build everything a user sees and interacts with on a website.</p>
+            <p>Back-end developers are responsible for the behind-the-scenes code that controls how a website loads and runs. They use languages like Python, Ruby, and PHP to build the server, the database, and the application that a website runs on.</p>
+            <p>Full-stack developers are familiar with both front-end and back-end languages, and they can build a website from start to finish all by themselves.</p>
+            <p>Web development is a high-growth career track, and many web developers work as freelancers. </p>
+            <p>Web development is a great career choice for those who want to work independently, or who want to work for a tech company. </p>
+            <p>Web development is a great career choice for those who want to work independently, or who want to work for a tech company. </p>
+            <p>Web development is a great career choice for those who want to work independently, or who want to work for a tech company. </p>
+            <p>Web development is a great career choice for those who want to work independently, or who want to work for a tech company. </p>
+            <p>Web development is a great career choice for those who want to work independently, or who want to work for a tech company. </p>
+            <p>Web development is a great career choice for those who want to work independently, or who want to work for a tech company. </p>
+            <p>Web development is a great career choice for those who want to work independently, or who want to work for a tech company. </p>
+
+
+        </div>
+    )
+}
